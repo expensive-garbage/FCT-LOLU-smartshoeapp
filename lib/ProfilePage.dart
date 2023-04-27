b418a577ed0d3790b876ed00f5c81e7ee8ecc6b9
Montignier <lola.montignier@telecomnancy.eu>
2023-04-27 16:56:01 +0200

    update profile page

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,56 +1,143 @@
 import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
   final int id = 0;
 
+  void _navigateToStatistics(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: [
-             Container(
-              //width: MediaQuery.of(context).size.width,
-              //height: MediaQuery.of(context).size.height*0.1,
-              color: const Color.fromRGBO(25, 131, 123, 1),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children : [
-                  const SizedBox(height: 10),
-                  Container(
-                  //width: MediaQuery.of(context).size.width,
-                  //height: MediaQuery.of(context).size.height*0.3,
-                  child: const CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30,
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.grey,
-                    ),
+      children: [
+        Container(
+          //width: MediaQuery.of(context).size.width,
+          //height: MediaQuery.of(context).size.height*0.1,
+          color: const Color.fromRGBO(25, 131, 123, 1),
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 10),
+              Container(
+                //width: MediaQuery.of(context).size.width,
+                //height: MediaQuery.of(context).size.height*0.3,
+                child: const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  child: Icon(
+                    Icons.person_3,
+                    color: Colors.grey,
                   ),
                 ),
-              
-              //const SizedBox(height: 10),
-              const Text(
-                'Lucie Boucher',
-                style: TextStyle(fontSize: 18,
-                fontWeight: FontWeight.bold)),
-          ]
-          ),
               ),
-             ),
-          const SizedBox(height: 16),
-          const Text(
-            'My profile overview',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold
-            ) ,
-          ),
 
+              //const SizedBox(height: 10),
+              const Text('Lucie Boucher',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ]),
+          ),
+        ),
+        const SizedBox(height: 16),
+        const Text(
+          'My profile overview',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 30),
+        Row(
+          children:  [
+            const Icon(
+              Icons.show_chart,
+              size: 40,
+              color: Color.fromRGBO(25, 131, 123, 1),
+            ),
+            const SizedBox(width: 20),
+            const Text(
+              'Statistics',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+            ),
+            const Expanded(child: SizedBox()),
+            ElevatedButton(
+              onPressed: ()=> _navigateToStatistics(context) , 
+              style : ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(20),
+                backgroundColor: const Color.fromARGB(255, 219, 129, 129)
+              ),
+              child: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size : 20,
+                color: Color.fromRGBO(202, 171, 236, 0.498),
+              )
+              ),
           ],
-        )
-      );
+        ),
+        const SizedBox(height: 50),
+        Row(
+          children: [
+            const Icon(
+              Icons.settings,
+              size: 40,
+              color: Color.fromRGBO(25, 131, 123, 1),
+            ),
+            const SizedBox(width: 20),
+            const Text(
+              'Settings',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+            ),
+            const Expanded(child: SizedBox()),
+            ElevatedButton(
+              onPressed: ()=> _navigateToStatistics(context) , 
+              style : ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(20),
+                backgroundColor: const Color.fromARGB(255, 219, 129, 129)
+              ),
+              child: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size : 20,
+                color: Color.fromRGBO(202, 171, 236, 0.498),
+              ),
+              ),
+          ],
+        ),
+        const SizedBox(height: 50),
+        Row(
+          children: [
+            const Icon(
+              Icons.people,
+              size: 40,
+              color: Color.fromRGBO(25, 131, 123, 1),
+            ),
+            const SizedBox(width: 20),
+            const Text(
+              'Change Profile',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+            ),
+            const Expanded(child: SizedBox()),
+            ElevatedButton(
+              onPressed: ()=> _navigateToStatistics(context) , 
+              style : ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(20),
+                backgroundColor: const Color.fromARGB(255, 219, 129, 129)
+              ), 
+              child: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size : 20,
+                color: Color.fromRGBO(202, 171, 236, 0.498),
+              
+              ),
+              ),
+          ],
+        ),
+      ],
+    ));
   }
 }
