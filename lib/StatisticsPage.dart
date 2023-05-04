4911f518c8d9fe8ba06077f54d6ba6dcb7dc0de3
Montignier <lola.montignier@telecomnancy.eu>
2023-05-04 16:45:21 +0200

    Statistics Page Scrollable

--- lib/StatisticsPage.dart
+++ lib/StatisticsPage.dart
@@ -1,11 +1,10 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:line_chart/charts/line-chart.widget.dart';
+//import 'package:line_chart/charts/line-chart.widget.dart';
 //import 'MyHomePage.dart';
 //import 'MyApp.dart';
 import 'package:pie_chart/pie_chart.dart';
-import 'package:line_chart/line_chart.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StatisticsPage extends StatefulWidget {
@@ -47,56 +46,70 @@ class StatisticsPageState extends State<StatisticsPage> {
     int currentIndex = 0;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: const Color.fromRGBO(25, 131, 123, 1),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  SizedBox(height: 16),
-                  Text('Statistics',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 16),
-                ],
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minHeight: MediaQuery.of(context).size.height,)
+        ,
+          child: Padding(padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                color: const Color.fromRGBO(25, 131, 123, 1),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      SizedBox(height: 16),
+                      Text('Statistics',
+                          style:
+                              TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 16),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              const SizedBox(height: 16),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: PieChart(
+                  dataMap: dataMap,
+                  chartType: ChartType.disc,
+                  ringStrokeWidth: 6,
+                  baseChartColor: Colors.grey[300]!,
+                  colorList: colorList,
+                ),
+              ),
+              const Text('Wearing days of each shoes',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
+              const SizedBox(height: 16),
+              Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  width: double.infinity,
+                  height: 200,
+                  child: SfCartesianChart(
+                    primaryXAxis: CategoryAxis(),
+                    series: <ChartSeries>[
+                      LineSeries<ChartData, String>(
+                          dataSource: [
+                            ChartData('Day 1', 0.2),
+                            ChartData('Day 2', 0.8),
+                            ChartData('Day 3', 0.6),
+                            ChartData('Day 4', 0.3)
+                          ],
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y)
+                    ],
+                  )
+                  ),
+              const Text('Humidity Rate',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
+                  const SizedBox(height: 16),
+            
+
+                ]),
           ),
-          const SizedBox(height: 16),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: PieChart(
-              dataMap: dataMap,
-              chartType: ChartType.disc,
-              ringStrokeWidth: 6,
-              baseChartColor: Colors.grey[300]!,
-              colorList: colorList,
-            ),
-          ),
-          const Text('Wearing days of each shoes',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal)),
-          const SizedBox(height: 16),
-          Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              width: double.infinity,
-              height: 200,
-              child: SfCartesianChart(
-                primaryXAxis: CategoryAxis(),
-                series: <ChartSeries>[
-                  LineSeries<ChartData, String>(
-                      dataSource: [
-                        ChartData('1', 0.2),
-                        ChartData('2', 0.8),
-                        ChartData('3', 0.6),
-                        ChartData('4', 0.3)
-                      ],
-                      xValueMapper: (ChartData data, _) => data.x,
-                      yValueMapper: (ChartData data, _) => data.y)
-                ],
-              ))
-        ],
+        ),
+        
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
